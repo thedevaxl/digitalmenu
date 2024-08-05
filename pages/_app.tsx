fd8b8856cec382b89555967c02b42cc027fdd7a7
devarrasgroup <developer@arrasgroupspa.com>
2024-08-06 00:31:21 +0200

    feat: plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../app/globals.css'
 import type { AppProps } from 'next/app'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
+  return (
+    <PlausibleProvider domain={baseUrl}>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  )
 }
 
 export default MyApp
