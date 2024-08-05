ce22fb25303d9efceaf3aba004377691fd69e168
devarrasgroup <developer@arrasgroupspa.com>
2024-08-06 01:03:31 +0200

    chore: debugging plausible 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import PlausibleProvider from "next-plausible";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Your Digital Menu Effortlessly",
-  description: "Join our platform and start offering a seamless ordering experience to your customers.",
+  description:
+    "Join our platform and start offering a seamless ordering experience to your customers.",
 };
 const plausibleDomain = "allrestaurants.menu";
 
@@ -19,14 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <Head>
-      <script
-        defer
-        data-domain={plausibleDomain}
-        src="https://plausible.io/js/script.js"
-      ></script>
-    </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PlausibleProvider domain={plausibleDomain}>
+          {children}
+        </PlausibleProvider>
+      </body>
     </html>
   );
 }
